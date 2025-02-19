814f57de2067209c4028d89c1ccc1e4822d74fa3
Zach Quita <zachquita@gmail.com>
2025-02-19 21:25:03 +0800

    Print area and perimiter of rectangles

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -7,7 +7,22 @@ public class Rectangle {
         height = recHeight;
     }
 
-    public static void main(String[] args) {
+    public static double area(Rectangle rectangle) {
+        return rectangle.width * rectangle.height;
+    }
 
+    public static double perimeter(Rectangle rectangle) {
+        return 2 * (rectangle.width + rectangle.height);
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle1 = new Rectangle(4.0, 3.0);
+        Rectangle rectangle2 = new Rectangle(1.5, 3.5);
+
+        System.out.println("Rectangle 1 Area: " + area(rectangle1));
+        System.out.println("Rectangle 1 Perimeter: " + perimeter(rectangle1));
+        System.out.print("\n");
+        System.out.println("Rectangle 2 Area: " + area(rectangle2));
+        System.out.println("Rectangle 2 Perimeter: " + perimeter(rectangle2));
     }
 }
