b8745f47ce5ed9fa4261389c72d387d019f3f912
Zach Quita <zachquita@gmail.com>
2025-02-19 14:59:40 +0800

    Add personInfo then Person constructor

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,9 +1,24 @@
 public class Person {
+    // Define attributes for Person
     private String name;
     private int age;
     private String address;
 
+    // Define constructor for Person
+    public Person(String personName, int personAge, String personAddress) {
+        name = personName;
+        age = personAge;
+        address = personAddress;
+    }
+
+    // Display all attributes for Person
+    public personInfo(Person person) {
+        System.out.println(person.name);
+        System.out.println(person.age);
+        System.out.println(person.address);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person donQuixote = new Person("Don Quixote", 30, "The Limbus");
     }
 }
