fb28cdf9ae6d03d69ca7bc957541c936567d8f19
Zach Quita <zachquita@gmail.com>
2025-02-19 15:57:17 +0800

    Initialize Dog.java

---
+++ src/main/java/Dog.java
@@ -0,0 +1,46 @@
+public class Dog {
+    private String name;
+    private  String breed;
+
+    public Dog(String dogName, String dogBreed) {
+        name = dogName;
+        breed = dogBreed;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+    public void setBreed(String newBreed) {
+        this.breed = newBreed;
+    }
+
+    public static void displayDog(Dog dog) {
+        System.out.println(dog.getName());
+        System.out.println(dog.getBreed());
+        System.out.println("-".repeat(dog.getBreed().length()));
+
+    }
+
+    public static void main(String[] args) {
+        Dog midnight = new Dog("Midnight", "Pomeranian");
+        Dog shadow = new Dog("Shadow", "German Shepard");
+
+        System.out.println("UNMODIFIED DOGS:");
+        displayDog(midnight);
+        displayDog(shadow);
+
+        midnight.setName("Snowy");
+        shadow.setBreed("Aspen");
+
+        System.out.println("MODIFIED DOGS:");
+        displayDog(midnight);
+        displayDog(shadow);
+    }
+}
