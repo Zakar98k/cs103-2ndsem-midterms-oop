5e9f9fb4ac7947143f6bbbcbdc6c536fe29b6e17
Zach Quita <zachquita@gmail.com>
2025-02-19 15:32:34 +0800

    Use arrayList, add displayInfo

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Person {
     // Define attributes for Person
     private String name;
@@ -11,14 +13,27 @@ public class Person {
         address = personAddress;
     }
 
-    // Display all attributes for Person
-    public void personInfo(Person person) {
-        System.out.println(person.name);
-        System.out.println(person.age);
-        System.out.println(person.address);
+    // Display all attributes for each Person
+    public static void displayPeople(ArrayList<Person> people) {
+        for (int i = 0; i < people.size(); i++) {
+            System.out.println("Person #" + Integer.toString(i+1));
+            System.out.println("Name: " + people.get(i).name);
+            System.out.println("Age: " + people.get(i).age);
+            // Only print newline if not the last person
+            if (i < people.size()-1)
+                System.out.print("\n");
+        }
     }
 
     public static void main(String[] args) {
-        Person donQuixote = new Person("Don Quixote", 30, "The Limbus");
+        ArrayList<Person> people = new ArrayList<Person>();
+
+        people.add(new Person("Don Quixote", 22, "The Limbus"));
+        people.add(new Person("Zach", 19, "Laguna, Philippines"));
+        people.add(new Person("Denji", 19, "Tokyo"));
+        people.add(new Person("Jeff Bezos", 61, "Washington, United States"));
+        people.add(new Person("Elon Musk", 54, "Mars"));
+
+        displayPeople(people);
     }
 }
