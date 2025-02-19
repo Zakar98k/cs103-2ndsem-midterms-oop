7889cf5cdfd3835f67a8d5b36508879d22b70c8b
Zach Quita <zachquita@gmail.com>
2025-02-19 18:17:15 +0800

    Init rectangle.java

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double recWidth, double recHeight) {
+        width = recWidth;
+        height = recHeight;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
