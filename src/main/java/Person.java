c0912149bf939e5acac1824f8851ee31acabebf4
Zach Quita <zachquita@gmail.com>
2025-02-19 13:41:58 +0800

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
